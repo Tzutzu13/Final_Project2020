6c2d019d76ebc6cf8de57d1d795500e433178027
יניב רוטרו <yanivrotaru13@gmail.com>
2020-03-01 18:38:32 +0200

    updated AE

---
+++ autoencoder_keras.py
@@ -0,0 +1,123 @@
+import os
+os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
+os.environ["CUDA_VISIBLE_DEVICES"]="1" #model will be trained on GPU 1
+import keras
+from matplotlib import pyplot as plt
+import numpy as np
+import gzip
+# %matplotlib inline
+from keras.layers import Input,Conv2D,MaxPooling2D,UpSampling2D
+from keras.models import Model
+from keras.optimizers import RMSprop
+
+train_data = np.reshape(train_imgs,(60000,28,28))
+test_data = np.reshape(test_imgs,(10000,28,28))
+
+# Shapes of training set
+print("Training set (images) shape: {shape}".format(shape=train_data.shape))
+
+# Shapes of test set
+print("Test set (images) shape: {shape}".format(shape=test_data.shape))
+
+# Create dictionary of target classes
+label_dict = {
+ 0: '0',
+ 1: '1',
+ 2: '2',
+ 3: '3',
+ 4: '4',
+ 5: '5',
+ 6: '6',
+ 7: '7',
+ 8: '8',
+ 9: '9',
+}
+
+plt.figure(figsize=[5,5])
+
+# Display the first image in training data
+plt.subplot(121)
+curr_img = np.reshape(train_data[0], (28,28))
+curr_lbl = train_labels[0]
+plt.imshow(curr_img, cmap='gray')
+plt.title("(Label: " + (label_dict[int(curr_lbl)]) + ")")
+
+# Display the first image in testing data
+plt.subplot(122)
+curr_img = np.reshape(test_data[0], (28,28))
+curr_lbl = test_labels[0]
+plt.imshow(curr_img, cmap='gray')
+plt.title("(Label: " + (label_dict[int(curr_lbl)]) + ")")
+    
+train_data = train_data.reshape(-1, 28,28, 1)
+test_data = test_data.reshape(-1, 28,28, 1)
+train_data.shape, test_data.shape
+train_data = train_data.astype(np.float32)
+test_data = test_data.astype(np.float32)   
+train_data.dtype, test_data.dtype
+ 
+from sklearn.model_selection import train_test_split
+train_X,valid_X,train_ground,valid_ground = train_test_split(train_data,
+                                                             train_data, 
+                                                             test_size=0.2, 
+                                                             random_state=13) 
+
+batch_size = 128
+epochs = 50
+inChannel = 1
+x, y = 28, 28
+input_img = Input(shape = (x, y, inChannel))
+
+def autoencoder(input_img):
+    #encoder
+    #input = 28 x 28 x 1 (wide and thin)
+    conv1 = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img) #28 x 28 x 32
+    pool1 = MaxPooling2D(pool_size=(2, 2))(conv1) #14 x 14 x 32
+    conv2 = Conv2D(64, (3, 3), activation='relu', padding='same')(pool1) #14 x 14 x 64
+    pool2 = MaxPooling2D(pool_size=(2, 2))(conv2) #7 x 7 x 64
+    conv3 = Conv2D(128, (3, 3), activation='relu', padding='same')(pool2) #7 x 7 x 128 (small and thick)
+
+    #decoder
+    conv4 = Conv2D(128, (3, 3), activation='relu', padding='same')(conv3) #7 x 7 x 128
+    up1 = UpSampling2D((2,2))(conv4) # 14 x 14 x 128
+    conv5 = Conv2D(64, (3, 3), activation='relu', padding='same')(up1) # 14 x 14 x 64
+    up2 = UpSampling2D((2,2))(conv5) # 28 x 28 x 64
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(up2) # 28 x 28 x 1
+    return decoded
+
+autoencoder = Model(input_img, autoencoder(input_img))
+autoencoder.compile(loss='mean_squared_error', optimizer = RMSprop())
+
+autoencoder.summary()
+autoencoder_train = autoencoder.fit(train_X, train_ground,
+                                    batch_size=batch_size,epochs=3,verbose=1,
+                                    validation_data=(valid_X, valid_ground))
+
+loss = autoencoder_train.history['loss']
+val_loss = autoencoder_train.history['val_loss']
+epochs = range(epochs)
+plt.figure()
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.legend()
+plt.show()
+
+pred = autoencoder.predict(test_data)
+
+pred.shape
+
+plt.figure(figsize=(20, 4))
+print("Test Images")
+for i in range(10):
+    plt.subplot(2, 10, i+1)
+    plt.imshow(test_data[i, ..., 0], cmap='gray')
+    curr_lbl = test_labels[i]
+    plt.title("(Label: " + str(label_dict[int(curr_lbl)]) + ")")
+plt.show()    
+plt.figure(figsize=(20, 4))
+print("Reconstruction of Test Images")
+for i in range(10):
+    plt.subplot(2, 10, i+1)
+    plt.imshow(pred[i, ..., 0], cmap='gray')  
+plt.show()
